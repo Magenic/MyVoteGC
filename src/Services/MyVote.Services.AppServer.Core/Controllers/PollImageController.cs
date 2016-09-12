e4b8fe824553125b937c20eda5c483afe3faab42
JPisaBrony <jdpohl789@gmail.com>
2016-09-12 10:45:07 -0500

    changed image upload code to use google's beta 2 storage

--- src/Services/MyVote.Services.AppServer.Core/Controllers/PollImageController.cs
+++ src/Services/MyVote.Services.AppServer.Core/Controllers/PollImageController.cs
@@ -2,10 +2,8 @@
 using Microsoft.AspNetCore.Mvc;
 using System;
 using System.IO;
-using System.Collections.Generic;
 using System.Threading.Tasks;
-using Google.Apis.Services;
-using Google.Apis.Storage.v1;
+using Google.Storage.V1;
 using Microsoft.AspNetCore.Http;
 
 namespace MyVote.Services.AppServer.Controllers
@@ -14,42 +12,23 @@ namespace MyVote.Services.AppServer.Controllers
     public sealed class PollImageController
         : Controller
     {
-        const string bucketName = "myvote";
+        const string bucketName = "";
 
         [Authorize]
         [HttpPost]
         public async Task<IActionResult> Post(IFormFile file)
         {
             string filename = file.FileName;
-            string content = file.ContentType;
+            string contentType = file.ContentType;
 
             BinaryReader reader = new BinaryReader(file.OpenReadStream());
             byte[] imageBytes = reader.ReadBytes((int)file.Length);
             var uploadStream = new MemoryStream(imageBytes);
 
-            var credentials = await Google.Apis.Auth.OAuth2.GoogleCredential.GetApplicationDefaultAsync();
+            var client = StorageClient.Create();
+            var uploaded = client.UploadObjectAsync(bucketName, filename, contentType, uploadStream);
 
-            if (credentials.IsCreateScopedRequired)
-            {
-                credentials = credentials.CreateScoped(new[] { StorageService.Scope.DevstorageFullControl });
-            }
-
-            var serviceInitializer = new BaseClientService.Initializer()
-            {
-                ApplicationName = "MyVote",
-                HttpClientInitializer = credentials
-            };
-
-            StorageService storage = new StorageService(serviceInitializer);
-
-            storage.Objects.Insert(
-                bucket: bucketName,
-                stream: uploadStream,
-                contentType: content,
-                body: new Google.Apis.Storage.v1.Data.Object() { Name = filename }
-            ).Upload();
-
-            return new OkObjectResult(new { imageUrl = "https://storage.googleapis.com/" + bucketName + "/" + filename});
+            return new OkObjectResult(new { imageUrl = "https://storage.googleapis.com/" + bucketName + "/" + filename });
         }
 
     }
